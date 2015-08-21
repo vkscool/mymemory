ac920ce15d40681dac70a0da2c4610f7b72d1ab5
Vikas Kumar <vikas.kumar@onmobile.com>
2015-08-21 15:04:13 +0530

    Initial commit from eclipse

--- src/main/java/com/mymemory/interfaces/Data.java
+++ src/main/java/com/mymemory/interfaces/Data.java
@@ -1,8 +1,5 @@
 package com.mymemory.interfaces;
 
-import java.sql.Timestamp;
-import java.util.List;
-
 public interface Data<K>{
 	public String getObjectAsString();
 	public K getObjectFromString(String s, Class<K> c);
