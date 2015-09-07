3f91802c6b7ce8605976b0521be32fb1a307b9c1
Vikas Kumar <vikas.kumar@onmobile.com>
2015-09-07 10:33:29 +0530

    Nothing just playing around

--- src/test/java/com/mymemory/test/TestJSON.java
+++ src/test/java/com/mymemory/test/TestJSON.java
@@ -72,7 +72,6 @@ public class TestJSON {
 						try {
 							Thread.sleep(1000);
 						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
 					}
